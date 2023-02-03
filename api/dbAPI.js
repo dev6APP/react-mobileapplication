45d57941ea397eed8f27af896379f61c99de48f8
Jonas Wouters <r0781280@student.thomasmore.be>
2023-02-03 11:05:37 +0100

    a

--- api/dbAPI.js
+++ api/dbAPI.js
@@ -117,15 +117,23 @@ class DbAPI {
     }
     // Post photodata
     static async addPhotoData(photoData){
+        console.log(photoData)
         try{
             await axios.post(baseUrl + "PhotoData", {
-                "fieldID": photoData.fieldID,
-                "amountFlowers": photoData.amountFlowers, 
-                "workerID": photoData.workerID, 
-                "date": photoData.date, 
-                "fieldOwnerID": photoData.fieldOwnerID,
-                "x": photoData.x,
-                "y": photoData.y
+                //"fieldID": photoData.fieldID,
+                //"amountFlowers": photoData.amountFlowers, 
+                //"workerID": photoData.workerID, 
+                //"date": photoData.date, 
+                //"fieldOwnerID": photoData.fieldOwnerID,
+                //"x": photoData.x,
+                //"y": photoData.y
+                "fieldID": 1,
+                "amountFlowers": 15, 
+                "workerID": 1, 
+                "date": "2023-02-03T09:50:31.303Z", 
+                "fieldOwnerID": 1,
+                "x": "4.9682609",
+                "y": "51.1573353"
             });
         } catch (err){
             console.log(err);
